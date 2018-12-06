1fe8da8e9bc123b30f06fb6038ac2cd13f628149
Anton Kvartalnyi <anton.kvart@gmail.com>
2018-12-06 17:14:07 +0200

    Initial commit

---
+++ src/ua/hillel/kvartalnyi/lesson01/task02/task02.java
@@ -0,0 +1,23 @@
+package ua.hillel.kvartalnyi.lesson01.task02;
+
+import java.util.Random;
+
+public class task02 {
+    public static void main(String[] args) {
+        int mas[][];
+        mas = new int[10][10];
+        Random ran = new Random();
+        for (int i = 0; i < mas.length; i++)
+            for (int j = 0; j < mas.length; j++) {
+                mas[i][j] = ran.nextInt(100);
+            }
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas.length; j++)
+
+                System.out.printf(mas[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
+
+
