3e34890da9714bdb4cab4f28ab9d58efd237eaa7
Anton Kvartalnyi <anton.kvart@gmail.com>
2018-12-13 19:39:20 +0200

    Название классов с большой буквы - исправил Оформить в отдельные методы - исправил

--- src/ua/hillel/kvartalnyi/lesson01/task02/Task02.java
+++ src/ua/hillel/kvartalnyi/lesson01/task02/Task02.java
@@ -4,13 +4,11 @@ import java.util.Random;
 
 public class Task02 {
     public static void main(String[] args) {
-        int mas[][];
-        mas = new int[10][10];
-        Random ran = new Random();
-        for (int i = 0; i < mas.length; i++)
-            for (int j = 0; j < mas.length; j++) {
-                mas[i][j] = ran.nextInt(100);
-            }
+        int[][] mas = getInts();
+        print(mas);
+    }
+
+    private static void print(int[][] mas) {
         for (int i = 0; i < mas.length; i++) {
             for (int j = 0; j < mas.length; j++)
 
@@ -18,6 +16,16 @@ public class Task02 {
             System.out.println();
         }
     }
-}
 
+    private static int[][] getInts() {
+        int mas[][];
+        mas = new int[10][10];
+        Random ran = new Random();
+        for (int i = 0; i < mas.length; i++)
+            for (int j = 0; j < mas.length; j++) {
+                mas[i][j] = ran.nextInt(100);
+            }
+        return mas;
+    }
+}
 
